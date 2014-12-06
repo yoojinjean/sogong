f19bf9d928707b060216a57b288afb44eeb72312
gangej <977800@naver.com>
2014-12-07 00:39:13 +0900

    student menu

---
+++ StudentMenu.h
@@ -0,0 +1,23 @@
+
+typedef struct stu
+{
+	char id[8];
+	char pw[5];
+	char name[20];
+	char department[40];
+
+}STU;
+
+void CreateID();
+int Create();
+void Student_Save_File(STU student[]);
+void Read_Student_File();
+void Count_Student();
+int Check_ID(char id[], int flag);
+int Check_PW(char pw[]);
+
+
+void StuLogin();
+int StuLogin_Menu();
+
+void StudentMenu();
