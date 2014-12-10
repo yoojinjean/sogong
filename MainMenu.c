e101c3dd98f58cdac292eae261d190c76fd9b440
dahyunkim <bring79@nate.com>
2014-12-10 12:24:55 +0900

    MODIFIED MAIN CFILE

---
+++ MainMenu.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <Windows.h>
+#include "MainMenu.h"
+#include "StudentMenu.h"
+#include "LibrarianMenu.h"
+
+
+int main()
+{
+	int menu;
+
+	while (1){
+
+		printf("Main Menu\n");
+		printf("1. Librarian mode\n2. Student mode\n3.Create ID\n0.Exit\n\nINPUT: ");
+		scanf("%d", &menu);
+		switch (menu)
+		{
+		
+		case 1:
+			Login_Check();
+			break;
+		case 2:
+			StuLogin();  
+			break;
+		case 3:
+			CreateID();
+			break;
+		case 0:
+			exit(0);
+
+		default:
+			printf("Wrong Number.\n\n");
+		}
+	}
+}
+
+int Login_Check(){
+int ok;
+
+   ok=LibLogin();
+
+   if(ok==0)
+   {
+      LibMenu();
+   }
+   else
+      printf("Wrong password");
+
+   return 0;
+}
